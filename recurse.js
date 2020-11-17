c268c503e5136d59913064354c91fc77b91116c7
mac <dylanmurphyDBA@gmail.com>
2020-11-16 22:20:42 -0800

    todo 3

--- recurse.js
+++ recurse.js
@@ -33,7 +33,27 @@ function rFactorial(max){
 
 }
 
-console.log(rFactorial(3));
-console.log(rFactorial(6.5));
+// console.log(rFactorial(3));
+// console.log(rFactorial(6.5));
 
-console.log(rFactorial(.45));
+// console.log(rFactorial(.45));
+
+
+
+// Recursive Binary Search
+// Given a sorted array and a value, recursively determine whether value is found within array. 
+
+// rBinarySearch([1,3,5,6],4) = false; 
+
+// rBinarySearch([4,5,6,8,12],5) = true.
+
+
+
+// Greatest Common Factor
+// Given two integers, create rGCF(num1,num2) to recursively determine Greatest Common Factor (the largest integer dividing evenly into both).
+//  Greek mathematician Euclid demonstrated these facts:
+
+// gcf(a,b) == a, if a == b;
+// gcf(a,b) == gcf(a-b,b), if a>b;
+// gcf(a,b) == gcf(a,b-a), if b>a.
+// Second: rework facts #2 and #3 to reduce stack consumption and expand rGCF’s reach. You should  be able to compute rGCF(123456,987654).
